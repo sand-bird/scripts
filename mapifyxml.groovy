a31e7e83ac2cd411570fffffc88a186f04e899cb
Ruby Vega <sandbird@tuta.io>
2016-06-27 16:18:46 -0400

    Create mapifyxml.groovy

---
+++ mapifyxml.groovy
@@ -0,0 +1,43 @@
+def groovyUtils = new com.eviware.soapui.support.GroovyUtils( context )
+def holder = groovyUtils.getXmlHolder( "" )
+holder.namespaces["ns"] = "http://github.com/sand-bird/toys/"
+
+def map = [:]
+
+
+def mapifyXml = { nodelist, container ->
+	nodelist.each {
+		if (it.hasChildNodes()) {
+			childMap = false
+			it.getChildNodes().each { if (it.hasChildNodes()) childMap = true }
+			
+			if (childMap) {
+				def newContainer = [:]
+				owner.call(it.getChildNodes(), newContainer)
+				if (!container.containsKey(it.localName)) {
+					container[it.localName] = newContainer
+				}
+				else if (container[it.localName] instanceof List) {
+					container[it.localName].push newContainer
+				}
+				else {
+					def newList = []
+					newList.push container[it.localName]
+					newList.push newContainer
+					container[it.localName] = newList
+				}
+			}
+			else {
+				owner.call(it.getChildNodes(), container)
+			}
+		}
+		else if (it.hasProperty('nodeValue') && it.nodeValue && it.nodeValue.trim() != "") {
+			container[it.getParentNode().localName] = it.nodeValue
+		}
+	}
+}
+
+
+for( item in holder.getDomNodes( "//ns:items" ) ) {
+	mapifyXml (item, map)
+}
